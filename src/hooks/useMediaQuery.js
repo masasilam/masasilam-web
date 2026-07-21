152f5b426a2dbd026f5112dc1ba6fc4b9bc8d3f6
MasasilaM <info@masa-silam.com>
2026-07-21 23:11:27 +0700

    Test push from develop

--- src/hooks/useMediaQuery.js
+++ src/hooks/useMediaQuery.js
@@ -16,9 +16,7 @@ export const useMediaQuery = (query) => {
     if (media.addEventListener) {
       media.addEventListener('change', listener)
       return () => media.removeEventListener('change', listener)
-    } 
-    // Fallback for older browsers
-    else {
+    } else {
       media.addListener(listener)
       return () => media.removeListener(listener)
     }
